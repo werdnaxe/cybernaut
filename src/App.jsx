0727f02b5a72e505e2795a67f7dfe6eb6d61a2d5
Daniel Szarkowicz <szarkod@wwu.edu>
2025-05-08 10:29:03 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <h1>
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
