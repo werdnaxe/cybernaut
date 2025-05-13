28d41c5f6d34bde54828dbd7e486165b9f9de3e5
LinneaEO <olixl@wwu.edu>
2025-05-13 12:59:13 -0700

    Setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import GradeLevel from './pages/GradeLevel'; // added
+import Destinations from './pages/Destinations'; // added
 
 const App = () => {
   return (
@@ -12,7 +14,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />} /> 
+        <Route path="/grade-level" element={<GradeLevel />} />
+        <Route path="/destinations" element={<Destinations />} /> 
       </Routes>
     </div>
   );
