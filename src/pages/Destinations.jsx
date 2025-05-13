65007d26c1a06d47ba581ae01e5fa5055592bd77
LinneaEO <olixl@wwu.edu>
2025-05-13 08:57:14 -0700

    Added destinations and updated grade level page to fit buttons correctly

---
+++ src/pages/Destinations.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Destinations = () => {
+  return (
+    <div className="bg-[url('src/assets/destinationsimage.png')] h-[1000px] flex justify-center items-center p-5">
+        <div className="flex flex-row items-center space-y-5">
+            <button
+                className="bg-[url('src/assets/mysterymountainbutton.png')] bg-cover bg-no-repeat border-none w-[440px] h-[440px] cursor-pointer focus:outline-none absolute top-30 left-220"
+                style={{
+                  clipPath: "ellipse(60% 15% at center)",
+                }}>
+            </button>
+            <button
+                className="bg-[url('src/assets/socialmediasafepassagebutton.png')] bg-cover bg-no-repeat border-none w-[440px] h-[440px] cursor-pointer focus:outline-none absolute top-160 left-30"
+                style={{
+                  clipPath: "ellipse(60% 15% at center)",
+                }}>
+            </button>
+            <button
+                className="bg-[url('src/assets/datadetoxpitbutton.png')] bg-cover bg-no-repeat border-none w-[440px] h-[440px] cursor-pointer focus:outline-none absolute top-166 left-183"
+                style={{
+                  clipPath: "ellipse(60% 15% at center)",
+                }}>
+            </button>
+        </div>
+    </div>
+  );
+};
+
+export default Destinations;
