1f3cf8028d3e18f9aad0bfd8a61d8a0d2e67a39d
Andrew Essex <Andrew.Essex@stmartin.edu>
2025-05-19 16:56:36 -0700

    added routes

--- server/api/routes/userRoutes.js
+++ server/api/routes/userRoutes.js
@@ -18,12 +18,47 @@ router.post('/', async (req, res) => {
     }
 })
 
+// Get all users
+router.get('/', async (req, res) => {
+    const user = await User.find();
+
+    if (!user) res.send('No users found').status(404);
+    else res.send(user.status(200));
+})
+
 // Get user by id
 router.get('/:id', async (req, res) => {
     const user = await User.findById(req.params.id);
 
     if (!user) res.send('User not found').status(404);
     else res.send(user).status(200);
+})
+
+// Update user by id
+router.put('/:id', async (req, res) => {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id);
+
+    if (!updatedUser) res.send('User not found').status(404);
+    else {
+        updatedUser.username = req.body.username;
+        updatedUser.email = req.body.email;
+        updatedUser.password = req.body.password;
+
+        await updatedUser.save();
+        console.log('User updated:', updatedUser);
+        res.send(updatedUser).status(200);
+    }
+})
+
+// Delete user by id
+router.delete('/:id', async (req, res) => {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+    if (!deletedUser) res.send('User not found').status(404);
+    else {
+        console.log('User deleted:', deletedUser);
+        res.send(deletedUser).status(200);
+    }
 })
 
 export default router;
