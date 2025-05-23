3f4190f25f629e6aa69d66ad8458058b7357e4d8
LinneaEO <olixl@wwu.edu>
2025-05-23 16:43:22 -0700

    Fixed button css

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -5,22 +5,16 @@ const Destinations = () => {
     <div className="bg-[url('src/assets/destinationsimage.png')] h-[1000px] flex justify-center items-center p-5">
         <div className="flex flex-row items-center space-y-5">
             <button
-                className="bg-[url('src/assets/mysterymountainbutton.png')] bg-cover bg-no-repeat border-none w-[440px] h-[440px] cursor-pointer focus:outline-none absolute top-30 left-220"
-                style={{
-                  clipPath: "ellipse(60% 15% at center)",
-                }} onClick={() => window.location.href = '/mysterymountain'}>
+                className="bg-[#537fb2] hover:bg-[#436b9a] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl absolute top-110 left-160" onClick={() => window.location.href = '/mysterymountain'}>
+                  Mountains of Mystery
             </button>
             <button
-                className="bg-[url('src/assets/socialmediasafepassagebutton.png')] bg-cover bg-no-repeat border-none w-[440px] h-[440px] cursor-pointer focus:outline-none absolute top-160 left-30"
-                style={{
-                  clipPath: "ellipse(60% 15% at center)",
-                }} onClick={() => window.location.href = '/SocialMediaPassage'}>
+                className="bg-[#90cddb] hover:bg-[#72acba] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl absolute top-210 left-50" onClick={() => window.location.href = '/SocialMediaPassage'}>
+                  Social Media Safe Passage
             </button>
             <button
-                className="bg-[url('src/assets/datadetoxpitbutton.png')] bg-cover bg-no-repeat border-none w-[440px] h-[440px] cursor-pointer focus:outline-none absolute top-166 left-183"
-                style={{
-                  clipPath: "ellipse(60% 15% at center)",
-                }} onClick={() => window.location.href = '/DataDetoxPit'}>
+                className="bg-[#7ba0ba] hover:bg-[#67889e] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl absolute top-215 left-210" onClick={() => window.location.href = '/DataDetoxPit'}>
+                  Data Detox Pit
             </button>
         </div>
     </div>
