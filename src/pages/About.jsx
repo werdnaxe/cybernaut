6989bc2423a8667405e00f72dfd3d285ca2c9b09
char-lotte-anne <larson95@wwu.edu>
2025-05-26 12:09:53 -0700

    quiz component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Quiz from '../components/Quiz';
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  // const handleAnswer = (result) => {
+  //   console.log('Quiz Result:', result); // true/false or selected option
+  // };
 
-export default About
+  return (
+    <div className="relative">
+      <div className="p-6">
+        <h1 className="text-3xl font-bold">About</h1>
+      </div>
+
+      {/* Quiz component below it */}
+      {/* <Quiz
+        options={['Cyberbullying', 'Privacy Settings', 'Algorithms', 'Digital Footprint']}
+        correctAnswer="Digital Footprint"
+        onAnswer={handleAnswer}
+      /> */}
+    </div>
+  );
+};
+
+export default About;
