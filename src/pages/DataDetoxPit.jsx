cd5e165f94b225bd71045a2d8c6dc021990abb5c
LinneaEO <olixl@wwu.edu>
2025-05-26 09:54:25 -0700

    Added module buttons to each destination and a back button that sends user back to destinations page

--- src/pages/DataDetoxPit.jsx
+++ src/pages/DataDetoxPit.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const DataDetoxPit = () => {
   return (
-    <div>DataDetoxPit</div>
+    <div className="bg-blue-200 min-h-screen w-full flex items-center justify-center">
+      {/* Back button */}
+      <button
+        className="bg-white hover:bg-grey text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl absolute top-40 left-20" onClick={() => window.location.href = '/destinations'}>
+           Back
+      </button>
+      {/* Module buttons */}
+      <div className="flex flex-col items-center space-y-6">
+        <button
+          className="bg-[#7ba0ba] hover:bg-[#67889e] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
+          Module 1
+        </button>
+        <button
+          className="bg-[#7ba0ba] hover:bg-[#67889e] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
+          Module 2
+        </button>
+        <button
+          className="bg-[#7ba0ba] hover:bg-[#67889e] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
+          Module 3
+        </button>
+        <button
+          className="bg-[#7ba0ba] hover:bg-[#67889e] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
+          Module 4
+        </button>
+      </div>
+    </div>
   )
 }
 
--- src/pages/MtMystery.jsx
+++ src/pages/MtMystery.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-const mtmystery = () => {
+const MtMystery = () => {
   return (
-    <div>About</div>
+    <div className="bg-blue-200 min-h-screen w-full flex items-center justify-center relative">
+      {/*Back button*/}
+      <button
+        className="bg-white hover:bg-grey text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl absolute top-20 left-20"
+        onClick={() => window.location.href = '/destinations'}>
+        Back
+      </button>
+      {/*Module buttons*/}
+      <div className="flex flex-col items-center space-y-6">
+        <button
+          className="bg-[#537fb2] hover:bg-[#436b9a] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
+          Module 1
+        </button>
+        <button
+          className="bg-[#537fb2] hover:bg-[#436b9a] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
+          Module 2
+        </button>
+        <button
+          className="bg-[#537fb2] hover:bg-[#436b9a] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
+          Module 3
+        </button>
+        <button
+          className="bg-[#537fb2] hover:bg-[#436b9a] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
+          Module 4
+        </button>
+      </div>
+    </div>
   )
 }
 
-export default mtm
+export default MtMystery
