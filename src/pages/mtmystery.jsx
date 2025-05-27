96957db239e45d0a49dc3ac44a32b1e70125ca58
werdnaxe <ajessex95@gmail.com>
2025-05-26 17:57:20 -0700

    removed from git tracking

--- src/pages/mtmystery.jsx
+++ src/pages/mtmystery.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MtMystery = () => {
   return (
-    <div className="relative bg-[url('src/assets/mtmysterypath.png')] min-h-screen w-full">
+    <div className="relative bg-[url('src/assets/mtmysterypath.png')] bg-top bg-no-repeat min-h-[100vh] w-full p-5">
       {/* Back button */}
       <button
         className="absolute top-10 left-10 bg-white hover:bg-grey text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl"
@@ -11,19 +11,19 @@ const MtMystery = () => {
       </button>
       {/* Module buttons - each with its own absolute position */}
       <button
-        className="absolute top-100 left-147 bg-[#90cddb] hover:bg-[#72acba] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
+        className="absolute top-100 left-155 bg-[#537fb2] hover:bg-[#436b9a] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
         Module 1
       </button>
       <button
-        className="absolute top-120 left-178 bg-[#90cddb] hover:bg-[#72acba] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
+        className="absolute top-125 left-140 bg-[#537fb2] hover:bg-[#436b9a] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
         Module 2
       </button>
       <button
-        className="absolute top-145 left-155 bg-[#90cddb] hover:bg-[#72acba] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
+        className="absolute top-150 left-170 bg-[#537fb2] hover:bg-[#436b9a] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
         Module 3
       </button>
       <button
-        className="absolute top-170 left-178 bg-[#90cddb] hover:bg-[#72acba] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
+        className="absolute top-173 left-133 bg-[#537fb2] hover:bg-[#436b9a] text-black font-zing font-bold py-3 px-10 rounded-full text-center option-button text-2xl">
         Module 4
       </button>
     </div>
